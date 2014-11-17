f27adb1b43efa5429bc0b2ffdc8a67ce26a794d0
Rick DeJonghe <raskalnikov@hotmail.com>
2014-11-17 16:00:42 -0600

    initial commit

---
+++ run_analysis.R
@@ -0,0 +1,22 @@
+
+### This file runs the analysis for the course project
+### Author - Rick DeJonghe
+
+### Course info
+### Coursera - Getting and Cleaning Data
+### by Jeff Leek, PhD, Roger D. Peng, PhD, Brian Caffo, PhD
+
+### Note - all data is contained in the data directory, but this is not included in the repo.
+
+
+########################################
+##### IMPORT RAW DATA
+########################################
+
+
+
+
+
+########################################
+##### FORM TIDY DATA SET
+########################################
